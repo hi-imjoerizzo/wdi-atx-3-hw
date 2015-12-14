3c050b0e567962ffcbd0aeead7a7ab828773aec1
Joe Rizzo <hi.imjoerizzo@gmail.com>
2015-12-14 15:28:48 -0600

    forgotten staged files

---
+++ w02/d04/morning/starter/js/main.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+  $('div').css("background-color", "blue");
+  $('div.hello').css('background-color', 'red');
+  $('body').append("<p>Adding a new paragraph</p>");
+
+  $('div. hello: first'). attr('class', 'goodbye')
+
+
+})
