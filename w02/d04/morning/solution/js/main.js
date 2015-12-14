aef0df20425f9eaf41f5c08e549a9643cac067dd
Joe Rizzo <hi.imjoerizzo@gmail.com>
2015-12-14 15:31:22 -0600

    files

---
+++ w02/d04/morning/solution/js/main.js
@@ -0,0 +1,20 @@
+$("document").ready(function(){
+	// $("div").css("background-color", "blue");
+	// $("div.hello").css("background-color", "red");
+	$("body").append(html);
+	var html = "<p>kjhejka</p>"
+	div.click().addClass("newClass")
+
+})
+
+// $("document").ready(function() {
+//      $("body").html("<p>The page just loaded!</p>");
+// });
+
+// window.addEventListener("DOMContentLoaded", function(evt) {
+//             var elem = document.getElementsByTagName("body")[0];
+//             var para = document.createElement("p");
+//             var text = document.createTextNode("The page just loaded!");
+//             para.appendChild(text);
+//             elem.appendChild(para);
+//         });
