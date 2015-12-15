0815e5f3fcf139c08c3e1e1271dff10aaf4aabd8
Matt Duncan <busman84@gmail.com>
2015-12-15 09:48:24 -0600

    adding Morning Exercises

---
+++ w03/d02/morning/starter/js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+	var count = 1;
+	function button1(){
+		$("#button1").html(count);
+		count++;
+	}
+	function button2(){
+		$("#button2").html(count);
+		count+=5;
+		button1();
+	}
+	function button3(){
+		$("#button3").html(count);
+		count -= 3;
+		button2();
+	}
+
+
+	$("#button1").click(function(){
+		console.log('clicked1');
+		button1();
+	});
+	$("#button2").click(function(){
+		console.log('clicked2');
+		button2();
+		$(this).toggleClass("hello");
+	});
+	$("#button3").click(function(){
+		console.log('clicked3');
+		button3();
+	});
+
+
+});
